d46da4bcd781394adb5d7a9c42351871665314f6
kenny-poncio <kponcio@cari.net>
2026-01-20 22:49:40 -0600

    Boton de Escuchar post

--- header.php
+++ header.php
@@ -79,13 +79,24 @@
             </div>
 
             <?php if ( is_singular( 'blog' ) ) : ?>
-                <!-- Subtítulo del post centrado -->
-                <?php
-                $subtitle = vguate_get_blog_post_subtitle();
-                if ( $subtitle ) :
-                ?>
-                    <p class="site-header__post-subtitle"><?php echo esc_html( $subtitle ); ?></p>
-                <?php endif; ?>
+                <!-- Contenedor centrado: Subtítulo + Botón escuchar -->
+                <div class="site-header__post-center">
+                    <?php
+                    $subtitle = vguate_get_blog_post_subtitle();
+                    if ( $subtitle ) :
+                    ?>
+                        <p class="site-header__post-subtitle"><?php echo esc_html( $subtitle ); ?></p>
+                    <?php endif; ?>
+
+                    <!-- Botón de escuchar post -->
+                    <button class="site-header__listen-btn" type="button" aria-label="Escuchar el post">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M3 18v-6a9 9 0 0 1 18 0v6"></path>
+                            <path d="M21 19a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h3zM3 19a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2H3z"></path>
+                        </svg>
+                        <span class="site-header__listen-text">Escuchar el post</span>
+                    </button>
+                </div>
             <?php endif; ?>
 
             <!-- Navegación Principal -->
